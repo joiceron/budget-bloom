aca198f7e266fe69846ee68668f957d68330a60a
joiceron <joiceron@hotmail.com>
2025-03-21 01:07:08 -0300

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,18 +8,18 @@ export default function Header() {
   return (
     <header className="header">
       <nav className="header__container">
-        <NavLink to={"/"} className="header__logo">
+        <div to={"/"} className="header__logo">
           <img src={logo} alt="logo Instock" className="header__logo--icon" />
           <h1 className="header__logo--text">Budget Bloom</h1>
-        </NavLink>
-        <NavLink to={"/account"} className="account">
+        </div>
+        <div to={"/"} className="account">
           <p className="account__user-name">Joice C.</p>
           <img
             src={gear}
             alt="gear icon for going to account settings"
             className="account__icon"
           />
-        </NavLink>
+        </div>
       </nav>
     </header>
   );
